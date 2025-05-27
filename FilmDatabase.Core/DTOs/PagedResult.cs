ce3a417f64e6dc87e5e984458ba6029325312dbe
antoniov15 <antonio.vicas@student.unitbv.ro>
2025-05-27 20:42:01 +0300

    [update] paged results

--- FilmDatabase.Core/DTOs/PagedResult.cs
+++ FilmDatabase.Core/DTOs/PagedResult.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FilmDatabase.Core.DTOs
+﻿namespace FilmDatabase.Core.DTOs
 {
-    internal class PagedResult
+    public class PagedResult<T>
     {
+        public IEnumerable<T> Data { get; set; } = new List<T>();
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalRecords { get; set; }
+        public int TotalPages { get; set; }
+        public bool HasPreviousPage => PageNumber > 1;
+        public bool HasNextPage => PageNumber < TotalPages;
+
+        public PagedResult()
+        {
+        }
+
+        public PagedResult(IEnumerable<T> data, int pageNumber, int pageSize, int totalRecords)
+        {
+            Data = data;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+            TotalRecords = totalRecords;
+            TotalPages = (int)Math.Ceiling((double)totalRecords / pageSize);
+        }
     }
 }
